21ab9788e4144b7c028c91e888c682c54270b0b2
anushkashr <99557370+anushkashr@users.noreply.github.com>
2023-08-05 17:51:19 -0500

    Add project files.

---
+++ ViewModel/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce_WebApp.ViewModel
+{
+	public class Category
+	{
+		public int? CategoryId { get; set; }
+
+		public string? CategoryName { get; set; }
+
+		public DateTime CreatedDateTime { get; set; }
+	}
+}
